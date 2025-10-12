82268dfeef9a761e1ba4437904c989b926f4ff4e
Felix Kretke <f.kretke@gmail.com>
2025-10-12 10:11:44 +0200

    removed unused

---
+++ apps/csm/src/main.cpp
@@ -0,0 +1,10 @@
+#include "glad/glad.h"
+#include "ogl/VertexBuffer.hpp"
+#include <vector>
+
+int main()
+{
+  const VertexBuffer vbo{};
+  const std::vector<GLfloat> vertices{ 0.0, 0.0, 0.5, 1.0, 1.0, 0.0 };
+  vbo.bufferData(vertices, GL_STATIC_DRAW);
+}
