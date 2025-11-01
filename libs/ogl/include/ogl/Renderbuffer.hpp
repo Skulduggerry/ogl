1fc39f9c83ee0d841ad83eca59a651b9e4f86af1
Felix Kretke <f.kretke@gmail.com>
2025-11-01 18:37:47 +0100

    make renderbuffer use the new InternalFormats struct

--- libs/ogl/include/ogl/Renderbuffer.hpp
+++ libs/ogl/include/ogl/Renderbuffer.hpp
@@ -5,23 +5,17 @@
 
 class Framebuffer;
 
-// TODO: put into external file
-enum struct StorageFormats : GLenum {
-  DEPTH_COMPONENT32F = GL_DEPTH_COMPONENT32F,
-  DEPTH24_STENCIL8 = GL_DEPTH24_STENCIL8,
-};
-
 class Renderbuffer
 {
   GLuint m_id = 0;
 
-  StorageFormats m_format = StorageFormats::DEPTH_COMPONENT32F;
+  InternalFormat m_format = InternalFormat::DEPTH_COMPONENT32F;
   GLsizei m_width = 0;
   GLsizei m_height = 0;
 
 public:
   Renderbuffer();
-  Renderbuffer(StorageFormats format, GLsizei width, GLsizei height);
+  Renderbuffer(InternalFormat format, GLsizei width, GLsizei height);
   ~Renderbuffer();
   Renderbuffer(const Renderbuffer &other) = delete;
   Renderbuffer(Renderbuffer &&other) noexcept;
@@ -29,9 +23,9 @@ public:
   Renderbuffer &operator=(const Renderbuffer &other) = delete;
   Renderbuffer &operator=(Renderbuffer &&other) noexcept;
 
-  void storage(StorageFormats format, GLsizei width, GLsizei height);
+  void storage(InternalFormat format, GLsizei width, GLsizei height);
 
-  [[nodiscard]] StorageFormats getFormat() const { return m_format; }
+  [[nodiscard]] InternalFormat getFormat() const { return m_format; }
   [[nodiscard]] GLsizei getWidth() const { return m_width; }
   [[nodiscard]] GLsizei getHeight() const { return m_height; }
 
--- libs/ogl/src/ogl/Renderbuffer.cpp
+++ libs/ogl/src/ogl/Renderbuffer.cpp
@@ -12,7 +12,7 @@ GLuint createRenderbuffer()
 
 Renderbuffer::Renderbuffer() : m_id(createRenderbuffer()) {}
 
-Renderbuffer::Renderbuffer(const StorageFormats format, const GLsizei width, const GLsizei height) : Renderbuffer{}
+Renderbuffer::Renderbuffer(const InternalFormat format, const GLsizei width, const GLsizei height) : Renderbuffer{}
 {
   storage(format, width, height);
 }
@@ -29,7 +29,7 @@ Renderbuffer &Renderbuffer::operator=(Renderbuffer &&other) noexcept
   return *this;
 }
 
-void Renderbuffer::storage(const StorageFormats format, const GLsizei width, const GLsizei height)
+void Renderbuffer::storage(const InternalFormat format, const GLsizei width, const GLsizei height)
 {
   m_format = format;
   m_width = width;
